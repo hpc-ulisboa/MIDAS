f66c4a629321b5054b52be888105b0fdf14a8e2f
Martim Bento <martimbento@andreia.inesc-id.pt>
2025-12-18 10:38:18 +0000

    Updated architecture model scripts

---
+++ architectures/nonstreaming_array.py
@@ -0,0 +1,32 @@
+import argparse
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from mpa import *
+
+mpa = init_standard_cgra(rows=4, cols=4, StreamPorts='all', mergeIOs=False)
+
+#mpa.add_interconnect((1, 1), (2, 2), latency=1, export=True)
+
+mpa.add_funct_to_PEs("ADD")
+mpa.add_funct_to_PEs("SUB")
+mpa.add_funct_to_PEs("MUL")
+mpa.add_funct_to_PEs("ASHR")
+mpa.add_funct_to_PEs("PHI")
+mpa.add_funct_to_PEs("LOAD")
+mpa.add_funct_to_PEs("STORE")
+mpa.add_funct_to_PEs("ICMP")
+mpa.add_funct_to_PEs("BR")
+
+mpa.set_pe_registerFile_sizes(4)
+
+n_output_registers = 1
+mpa.set_all_pe_num_output_registers(n_output_registers)
+
+mpa.addRfReadPortsToAllPEs("FU", 1)
+mpa.addRfReadPortsToAllPEs("OutputRegisters", n_output_registers)
+
+mpa.plot()
+
+mpa.export()
+
